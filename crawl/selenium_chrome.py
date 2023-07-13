f61d6b2f6ce1e352b5c4f587c2b5dedca2d17939
yxzlwz <danny070601@aliyun.com>
2023-07-13 15:43:00 +0800

    [fix] add --no-sandbox

--- crawl/selenium_chrome.py
+++ crawl/selenium_chrome.py
@@ -17,6 +17,7 @@ class SeleniumChrome:
 
     def __init__(self):
         options = Options()
+        options.add_argument('--no-sandbox')
         options.add_argument('--headless')
         options.add_argument('--disable-gpu')
         options.add_argument(
