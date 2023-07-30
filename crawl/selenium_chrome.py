978f282825427bbd1ee19d66d0e25aa3ae3bf74c
root <root@hccserver.hccserver>
2023-07-31 01:36:29 +0800

    [fix]

--- crawl/selenium_chrome.py
+++ crawl/selenium_chrome.py
@@ -18,7 +18,7 @@ class SeleniumChrome:
     def __init__(self):
         options = Options()
         options.add_argument('--no-sandbox')
-        # options.add_argument('--headless')
+        options.add_argument('--headless')
         options.add_argument('--disable-gpu')
         options.add_argument(
             '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
